aef0c533858e4c8152445956d6127f2dbae2c653
onewkub <37097081+onewkub@users.noreply.github.com>
2018-04-11 20:05:51 +0700

    Game

---
+++ Red Running/Assets/player_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+	Rigidbody2D rb;
+	Animator am;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		am = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		am.SetBool ("isruning", true);
+		rb.velocity = new Vector2 (speed, rb.velocity.y);
+		
+	}
+}
