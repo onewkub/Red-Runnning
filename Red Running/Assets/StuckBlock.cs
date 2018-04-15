6463a3ff56cbb374ab4a7051b3e17be34e422457
onewkub <37097081+onewkub@users.noreply.github.com>
2018-04-15 15:52:05 +0700

    Fix Stuck Block Again

--- Red Running/Assets/StuckBlock.cs
+++ Red Running/Assets/StuckBlock.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class StuckBlock : MonoBehaviour {
 	// Use this for initialization
 	GameObject player;
-	float BlockPositX, BlockPositY ,CurrX,CurrY,HighBlock;
-	float SizePosX, SizePosY;
+	float BlockPositY ,CurrY,HighBlock;
+	float SizePosY;
 	void Start () {
 		player = GameObject.Find ("player");
-		SizePosX = GetComponent<BoxCollider2D> ().size.x;
 		SizePosY = GetComponent<BoxCollider2D> ().size.y;
-		BlockPositX = transform.position.x- (SizePosX/2);
 		BlockPositY = transform.position.y + (SizePosY / 2);
 		//Debug.Log (BlockPositX);
 		
@@ -19,12 +17,8 @@ public class StuckBlock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		CurrX = player.transform.position.x ;
 		CurrY = player.transform.position.y;
-		Debug.Log (CurrX);
-		if (CurrX > BlockPositX) {
-			player_controller.isStuck = false;
-		}
+		//Debug.Log (CurrX);
 		if(CurrY > BlockPositY){
 			player_controller.isStuck = false;
 		}
